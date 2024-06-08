c77ae2edd840f0b336eb313132920ec952f19364
sgallaf <jalal.gallaf@gmail.com>
2024-06-08 14:37:28 +0300

    Adding to repo

---
+++ src/main/java/com/restaurantsystem/hosptialapi/HosptialApiApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantsystem.hosptialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosptialApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HosptialApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restaurantsystem/hosptialapi/HosptialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantsystem.hosptialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosptialApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
